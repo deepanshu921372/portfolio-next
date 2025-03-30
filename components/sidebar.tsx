6b222d077035cf24604edca31b6692468e5277ba
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-03-30 12:57:51 +0530

    resume link fix

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -26,7 +26,7 @@ const navItems = [
   { name: "Projects", path: "/projects", icon: FolderKanban },
   { name: "Skills", path: "/skills", icon: Code2 },
   { name: "Contact", path: "/contact", icon: Mail },
-  { name: "Resume", path: "https://drive.google.com/file/d/1u3mAM1AuLdkK4c_zjRIxaLk-8WQEuzwV/view?usp=sharing", icon: File },
+  { name: "Resume", path: "https://drive.google.com/file/d/1TXsxuk-myyDz6MEec-g2mA3peAHYsqh6/view?usp=sharing", icon: File },
 ];
 
 export function Sidebar() {
